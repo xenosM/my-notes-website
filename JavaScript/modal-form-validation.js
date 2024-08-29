6ddd76c476aca28ad286eb8c484eb08d7b63a8d5
xenosM <105599352+xenosM@users.noreply.github.com>
2024-08-29 19:26:05 +0545

    login and signin form completed

--- JavaScript/modal-form-validation.js
+++ JavaScript/modal-form-validation.js
@@ -1,13 +1,68 @@
 const siginForm = document.querySelector('.signin-form')
+const loginForm = document.querySelector('.login-form')
 const signinName = document.querySelector('#signin-name') 
 const signinEmail = document.querySelector('#signin-email') 
 const signinPassword = document.querySelector('#signin-password') 
 const signinConform= document.querySelector('#signin-conform') 
-
-
-
-
+const loginName = document.querySelector('#login-name')
+const loginPassword = document.querySelector('#login-password')
 
 siginForm.onsubmit = (e)=>{
     e.preventDefault();
+    if(validateForm()){
+        alert('signin successful')
+        clearInput()
+    }
+    
+}
+loginForm.onsubmit = (e)=>{
+    e.preventDefault();
+    if(validateForm(true)){
+        alert('login successful')
+        clearInput()
+    }
+    
+}
+
+
+function validateForm(isLoginForm){
+    let dataArray=[]
+    isLoginForm?   dataArray = [loginName,loginPassword] :  dataArray = [signinName,signinEmail,signinPassword,signinConform]
+    let isValid = true
+
+    dataArray.forEach((data)=>{
+        if(!data.value.trim()){
+            showTooltip(`#tooltip-${data.id}`,'This field is required')
+            isValid = false;
+        }
+    })
+    if(isLoginForm){
+        return isValid
+    }
+    if(isValid && signinPassword.value !=signinConform.value){
+        showTooltip('.password-auth','Passwords doesn\'t match')
+        isValid = false;
+    }
+    return isValid
+}
+
+function showTooltip(selector,message){
+    console.log(selector);
+    let tooltip =document.querySelectorAll(selector)
+    tooltip.forEach((element)=>{
+        element.innerText = message
+        element.classList.add('show')
+        setTimeout(()=>{
+            element.classList.remove('show')
+        },4000)
+    })
+}
+
+function clearInput(){
+    document.querySelectorAll('.signin-input').forEach((element)=>{
+        element.value=""
+    })
+    document.querySelectorAll('.login-input').forEach((element)=>{
+        element.value=""
+    })
 }
--- JavaScript/modal.js
+++ JavaScript/modal.js
@@ -1,16 +1,26 @@
-let signinBtn = document.querySelector('.header-signin-btn');
+let headerBtn = document.querySelectorAll('.header-btn');
 let modalOutsideContainer= document.querySelector('.modal-outside-container')
-let modalClose = document.querySelector('.modal-close')
+let modalClose = document.querySelectorAll('.modal-close')
 let signinModalContainer = document.querySelector('.signin-modal-container')
+let loginModalContainer = document.querySelector('.login-modal-container')
 
-signinBtn.addEventListener('click',()=>{
-    modalOutsideContainer.classList.add('show-modal')
-    signinModalContainer.classList.add('show-modal-container')
-    document.body.classList.add('no-scroll')
+headerBtn.forEach((btn)=>{
+    console.log(btn);
+    btn.addEventListener('click',(e)=>{
+        modalOutsideContainer.classList.add('show-modal')
+        console.log(e.target.id);
+        e.target.id == 'signin-btn'?  signinModalContainer.classList.add('show-modal-container') :loginModalContainer.classList.add('show-modal-container')
+        document.body.classList.add('no-scroll')
+    })
+
 })
 
-modalClose.addEventListener('click',()=>{
-    modalOutsideContainer.classList.remove('show-modal')
-    signinModalContainer.classList.remove('show-modal-container')
-    document.body.classList.remove('no-scroll')
+modalClose.forEach((btn)=>{
+    btn.addEventListener('click',()=>{
+        modalOutsideContainer.classList.remove('show-modal')
+        signinModalContainer.classList.remove('show-modal-container')
+        loginModalContainer.classList.remove('show-modal-container')
+        document.body.classList.remove('no-scroll')
+    })
+
 })
